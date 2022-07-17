db3b80b80abfb23d47b4354297a1694cd7f9c1b5
Ilesh Thiada <ileshkt@gmail.com>
2022-07-17 23:26:05 +0530

    Fix style

--- Formula/ferium.rb
+++ Formula/ferium.rb
@@ -1,23 +1,22 @@
 class Ferium < Formula
-    desc "Fast and multi-source CLI program for managing Minecraft mods and modpacks"
-    homepage "https://github.com/gorilla-devs/ferium"
-    url "https://github.com/gorilla-devs/ferium/archive/v4.1.7.tar.gz"
-    sha256 "2c2193417aff8af9045ea87a6ec6a35d3d1a48bdfdef05fc520fc4b9877de547"
-    license "MPL-2.0"
-    head "https://github.com/gorilla-devs/ferium.git", branch: "main"
-  
-    depends_on "rust" => :build
-  
-    on_linux do
-      depends_on "pkg-config" => :build
-    end
-  
-    def install
-      system "cargo", "install", *std_cargo_args
-    end
-  
-    test do
-      system "ferium", "help"
-    end
+  desc "Fast and multi-source CLI program for managing Minecraft mods and modpacks"
+  homepage "https://github.com/gorilla-devs/ferium"
+  url "https://github.com/gorilla-devs/ferium/archive/v4.1.7.tar.gz"
+  sha256 "2c2193417aff8af9045ea87a6ec6a35d3d1a48bdfdef05fc520fc4b9877de547"
+  license "MPL-2.0"
+  head "https://github.com/gorilla-devs/ferium.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkg-config" => :build
   end
-  
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "ferium", "help"
+  end
+end
