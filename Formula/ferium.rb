db7ecfd57772d0761cc69089f94d4c93a680f244
Ilesh Thiada <ileshkt@gmail.com>
2022-07-17 23:19:21 +0530

    Sync branch (#2)

--- Formula/ferium.rb
+++ Formula/ferium.rb
@@ -1,22 +1,16 @@
 class Ferium < Formula
   desc "Fast and multi-source CLI program for managing Minecraft mods and modpacks"
   homepage "https://github.com/gorilla-devs/ferium"
-  url "https://github.com/gorilla-devs/ferium/archive/v4.1.7.tar.gz"
-  sha256 "2c2193417aff8af9045ea87a6ec6a35d3d1a48bdfdef05fc520fc4b9877de547"
+  url "https://github.com/gorilla-devs/ferium/releases/download/v4.1.5/ferium-macos-x64.zip"
+  version "4.1.5"
+  sha256 "2bf189e1602677eaf8ed9823620ef8e8d03271cd3e7372e9a70ffecc5426d8c9"
   license "MPL-2.0"
-  head "https://github.com/gorilla-devs/ferium.git", branch: "main"
-
-  depends_on "rust" => :build
-
-  on_linux do
-    depends_on "pkg-config" => :build
-  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "ferium"
   end
 
   test do
-    system "ferium", "help"
+    system "ferium", "-V"
   end
 end
