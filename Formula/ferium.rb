027259fd6df327c3c7b2db17bb7042322693de80
Ilesh Thiada <ileshkt@gmail.com>
2022-07-17 23:07:45 +0530

    Compile from source

--- Formula/ferium.rb
+++ Formula/ferium.rb
@@ -1,16 +1,22 @@
 class Ferium < Formula
   desc "Fast and multi-source CLI program for managing Minecraft mods and modpacks"
   homepage "https://github.com/gorilla-devs/ferium"
-  url "https://github.com/gorilla-devs/ferium/releases/download/v4.1.5/ferium-macos-x64.zip"
-  version "4.1.5"
-  sha256 "2bf189e1602677eaf8ed9823620ef8e8d03271cd3e7372e9a70ffecc5426d8c9"
+  url "https://github.com/gorilla-devs/ferium/archive/v4.1.7.tar.gz"
+  sha256 "2c2193417aff8af9045ea87a6ec6a35d3d1a48bdfdef05fc520fc4b9877de547"
   license "MPL-2.0"
+  head "https://github.com/gorilla-devs/ferium.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
 
   def install
-    bin.install "ferium"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
-    system "ferium", "-V"
+    system "ferium", "help"
   end
 end
