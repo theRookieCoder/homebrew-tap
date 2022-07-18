c0fb11e676390a5e97300c416194522cb4db8ce3
Ilesh Thiada <ileshkt@gmail.com>
2022-07-18 01:42:24 +0000

    ferium: add 4.1.7 bottle.

--- Formula/ferium.rb
+++ Formula/ferium.rb
@@ -6,6 +6,12 @@ class Ferium < Formula
   license "MPL-2.0"
   head "https://github.com/gorilla-devs/ferium.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/theRookieCoder/homebrew-tap/releases/download/ferium-4.1.7"
+    sha256 cellar: :any_skip_relocation, big_sur:      "d7470e41487527be63605951164a526cd55a26c4899dc7c121daf01985dd3bce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ea6e08985424e5837825cb3cd9c7257343ec2acd66c7e24f957f1494780e9136"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
